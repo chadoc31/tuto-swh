8993ba5bdf87ab0fbf7f43d76a6bac491b898b59
chadoc31 <chagirard22@mailfence.com>
2025-10-14 11:15:00 +0200

    Ajout d'un fichier code Python

---
+++ my_code.py
@@ -0,0 +1,6 @@
+https://github.com/cyril-allignol
+https://github.com/cyril-allignol/paparazzi
+
+
+swh:1:cnt:9cda33fba4cd4001b3028a1f4b6b0748dcd78e61;origin=https://github.com/cyril-allignol/paparazzi;visit=swh:1:snp:fa3d0f0b0ac0244a46f650237919aff72c50e48b;anchor=swh:1:rev:e7c67e2072838e3834be45331417ce88845d83d3;path=/src/paparazzi
+print("Hello all ! merci pour votre présence à ce séminaire !)"
